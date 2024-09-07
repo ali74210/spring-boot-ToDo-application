365dfec8283f63309902991fe63ecaf4170d1e40
ali74210 <ali.toliat80@gmail.com>
2024-09-07 10:03:58 +0330

    initialize project dependencies

---
+++ src/test/java/com/sat/springboottodoapplication/SpringBootToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sat.springboottodoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
